ccff276c5bef6a09d0addb29cdc11658d4f159b8
ridd0053 <ridd0053@hz.nl>
2022-04-14 13:36:10 +0200

    Start of the product and categories pages - video 105

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -1,9 +1,60 @@
-exports.createPages = async ({ actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
+  const result = await graphql(
+    `
+     {
+      products: allStrapiProduct {
+        edges {
+          node {
+            name
+            strapiId
+            category {
+              name
+            }
+          }
+        }
+      }
+      categories: allStrapiCategory(filter: {}) {
+        edges {
+          node {
+            strapiId
+            name
+            description
+          }
+        }
+      }
+    } 
+    `
+    )
+
+    if(result.error) {
+      throw result.errors
+    }
+    const products = result.data.products.edges
+    const categories = result.data.categories.edges
+
+    products.forEach(product => {
+      createPage({
+        path: `/${product.node.category.name.toLowerCase()}/${
+          product.node.name.split(" ")[0]
+        }`,
+        component: require.resolve("./src/templates/product-detail.js"),
+        context: {
+          name: product.node.name,
+          category: product.node.category.name,
+          id: product.node.strapiId,
+        },
+      })
+    });
+    categories.forEach(category => {
+      createPage({
+        path: `/${category.node.name.toLowerCase()}`,
+        component: require.resolve("./src/templates/product-list.js"),
+        context: {
+          name: category.node.name,
+          description: category.node.description,
+          id: category.node.strapiId,
+        },
+      })
+    });
 }
